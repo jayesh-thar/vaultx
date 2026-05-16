62743f2a27cdd8ca1d03228ee8ae4fabf5f92cf5
Jayesh Thar <jayeshthar1409@gmail.com>
2026-05-16 12:57:40 +0530

    chore(api): configure ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+];
